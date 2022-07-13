4bd37b8c577662aaa918d6dc2a83b4f535a452ed
Anton Komarnytskyi <anton.komarnytskyi@masterofcode.com>
2022-07-13 14:04:34 +0300

    make request to database

--- api/src/index.js
+++ api/src/index.js
@@ -1,13 +1,29 @@
 const express = require('express');
 const { connectDb } = require('./helpers/db');
 const { host, port, db } = require('./configuration');
+const mongoose = require('mongoose');
 const app = express();
+const postSchema = new mongoose.Schema({
+  name: String
+});
+const Post = mongoose.model('Post', postSchema);
 
 const startServer = () => {
   app.listen(port, () => {
     console.log(`Started api service on ${port}`);
     console.log(`On host ${host}`);
     console.log(`Database ${db}`);
+
+    // Post.find((err,posts) => {
+    //   if(err) return console.error(err);
+    //   console.log('posts',posts);
+    // })
+
+    const silence = new Post({name: "Silence"});
+    silence.save((err, savedSilence) => {
+      if(err) return console.error(err);
+      console.log({savedSilence});
+    })
   });
 }
 
